e324d98006f2be8b053cf74a555184104fccfb2f
ccontre03 <christophercontreras23@gmail.com>
2026-02-11 02:39:47 -0600

    fonts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-linear-to-br from gray-900 via-gray-800 to-gray-900 donr-sans">
+  <div class="min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 donr-sans">
     <RouterView />
   </div>
 </template>
