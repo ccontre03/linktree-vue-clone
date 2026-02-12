de65a7077d2504a06272755a30df1a63fea67d56
ccontre03 <christophercontreras23@gmail.com>
2026-02-11 19:06:45 -0600

    Added profile and basic link

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,30 @@
 <script setup>
-//import { ref } from 'vue'
+import { ref } from 'vue'
 import LinkCard from '@/components/LinkCard.vue';
+
+const profile = ref({
+  name: 'Christopher Contreras',
+  slogan: 'Full stack Developer and Teacher',
+  avatar: "https://api.dicebear.com/9.x/adventurer/svg?seed=Milo",
+
+})
 </script>
 
 <template>
-  <main class="flex min-h-screen flex-col items-center px-4 pt-8">
+  <main class="flex min-h-screen flex-col items-center px-4 py-8">
     <!-- Profile Header -->
-     <dev>
-      <h>Christopher Contreras</h>
-     </dev>
+     <div class="mb-8 flex flex-col items-center text center">
+      <div 
+        class="mb-4 h-24 w-24 overflow-hidden rounded-full border-4 border-green-400 shadow-lg shadow-green-500/20 sm:h-32 sm:w-32">
+        
+        <img 
+          :src="profile.avatar" 
+          :alt="profile.name"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <h1 class="mb-2 font-display text-2xl font-bold text-white sm:text-3xl">{{ profile.name }}</h1>
+      <p class="max-w-xs text-gray-300 sm:text-lg">{{ profile.slogan }}</p>
+     </div>
   </main>
 </template>
